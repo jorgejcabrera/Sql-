dc84ebb2c1f5b30506348389c4e7f7ad99dcfae8
jorgecabrera <jorge.cabrera@mercadolibre.com>
2016-11-18 19:43:23 -0300

    fix

--- Ejercicio_2/queries.sql
+++ Ejercicio_2/queries.sql
@@ -1,10 +1,12 @@
 
 /* servicios que brinden especialidades sin personal idoneo */
-select * 
-from ejercicio2.servicio_especialidad se1
-where se1.nombre_del_servicio in (select se2.nombre_del_servicio
-                                 from ejercicio2.servicio_especialidad se2, ejercicio2.medico_servicio ms, ejercicio2.medico_especialidad me
-                                 where se2.nombre_del_servicio = ms.nombre_del_servicio
-                                 and ms.matricula = me.matricula
-                                 and se2.nombre_de_especialidad = me.nombre_de_especialidad);
+select se1.nombre_del_servicio
+from ejercicio2.servicio_especialidad se1, ejercicio2.medico_servicio ms1
+where se1.nombre_del_servicio = ms1.nombre_del_servicio
+group by se1.nombre_del_servicio,ms1.matricula
+having ms1.matricula not in (select me.matricula
+							from ejercicio2.servicio_especialidad se, ejercicio2.medico_servicio ms, ejercicio2.medico_especialidad me
+							where me.nombre_de_especialidad = se.nombre_de_especialidad
+							and me.matricula = ms.matricula
+							and se.nombre_del_servicio = ms.nombre_del_servicio);
                                  
