d6aab074cfae98e01f3fdc0ca208bac70a5b501c
jorgecabrera <jorge.cabrera@mercadolibre.com>
2016-11-19 17:42:32 -0300

    termino parcial 2

--- Ejercicio_2/queries.sql
+++ Ejercicio_2/queries.sql
@@ -10,3 +10,16 @@ having ms1.matricula not in (select me.matricula
 							and me.matricula = ms.matricula
 							and se.nombre_del_servicio = ms.nombre_del_servicio);
                                  
+/* servicios que ofrezcan mas de 3 especialidades y sean atendidos por mas de dos medicos */
+select se1.nombre_del_servicio
+from ejercicio2.servicio_especialidad se1
+group by se1.nombre_del_servicio
+having count(*) > 3
+
+intersect
+
+select se2.nombre_del_servicio
+from ejercicio2.servicio_especialidad se2, ejercicio2.medico_servicio ms
+where se2.nombre_del_servicio = ms.nombre_del_servicio
+group by se2.nombre_del_servicio, ms.matricula
+having count(*)>2;                                 
