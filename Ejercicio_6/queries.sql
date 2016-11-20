1c54ad3e39791cc65171bd17ef2a00e7755a25da
jorgecabrera <jorge.cabrera@mercadolibre.com>
2016-11-20 18:34:49 -0300

    segunda query

--- Ejercicio_6/queries.sql
+++ Ejercicio_6/queries.sql
@@ -18,3 +18,18 @@ where cli.cod_cli in (select cli1.cod_cli
                       and de2.cod_art = ar2.cod_art
                       and ar2.cod_rubro = ru2.cod_rubro
                       and ru2.nombre_del_rubro = 'Electricidad');
+                      
+/* listar codigos de rubros, nombree_del_rubro y el total facturado por rubro, para aquellos rubros que hayan 
+facturado mas de $9000 durante el mes de enero de 2015 */
+
+select ru.cod_rubro,ru.nombre_del_rubro, ar.cod_art, ar.precio, sum(de.cantidad)
+from ejercicio6.rubros ru, ejercicio6.facturas fa, ejercicio6.detalles de, ejercicio6.articulos ar
+where fa.nrofactura = de.nrofactura
+and de.cod_art = ar.cod_art
+and ar.cod_rubro = ru.cod_rubro
+and fa.fecha >= '20150101'
+and fa.fecha <= '20150131'
+group by ru.cod_rubro,ru.nombre_del_rubro, ar.cod_art, ar.precio
+having (ar.precio * sum(de.cantidad)) > 9000;
+
+
