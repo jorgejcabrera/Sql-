8966556511a9c14e2c0543e8db84d61aa6b897b4
jorgecabrera <jorge.cabrera@mercadolibre.com>
2016-11-20 15:28:23 -0300

    ejercicio 4

--- Ejercicio_4/queries.sql
+++ Ejercicio_4/queries.sql
@@ -6,3 +6,18 @@ where jo.nro_de_lic in (select co.nro_de_lic
 						where co.posicion <= 3
 						group by co.nro_de_lic
 						having count(*) > 5);
+                        
+/* cantidad de veces que en San Isidro compitieron juntos los jockeys con licencias 123 y 456 */
+select *
+from ejercicio4.encuentros en1
+where en1.nro_encuentro in (select en.nro_encuentro
+							from ejercicio4.encuentros en, ejercicio4.corredores co
+							where en.nro_encuentro = co.nro_encuentro
+							and en.nombre_hipo = 'San Isidro'
+							and co.nro_de_lic = 123
+							intersect
+							select en.nro_encuentro
+							from ejercicio4.encuentros en, ejercicio4.corredores co
+							where en.nro_encuentro = co.nro_encuentro
+							and en.nombre_hipo = 'San Isidro'
+							and co.nro_de_lic = 456);
